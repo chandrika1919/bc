5b98ad5afe85813fcadefca7bfd91655d0d534ff
chandrika1919 <chandrika9966m@gmail.com>
2025-06-12 15:35:55 +0530

    Create fFibonacci using tabulation.java

---
+++ fFibonacci using tabulation.java
@@ -0,0 +1,32 @@
+import java.util.HashMap;
+import java.util.Scanner;
+public class Main {
+    private HashMap<Integer, Integer> memo = new HashMap<>();
+    public Main() {
+        memo.put(0, 0);
+        memo.put(1, 1);
+    }
+    public int fibmemo(int n) {
+        if (memo.containsKey(n)) {
+            return memo.get(n);
+        }
+        int result = fibmemo(n - 1) + fibmemo(n - 2);
+        memo.put(n, result);
+        return result;
+    }
+    public static void main(String[] args) {
+        Main fibonacci = new Main();
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a non-negative integer: ");
+        int n = scanner.nextInt();
+        if (n < 0) {
+            System.out.println("Please enter a non-negative integer.");
+        } else {
+            System.out.printf("%-10s%-15s%n", "n", "Fibonacci(n)");
+            for (int i = 0; i <= n; i++) {
+                System.out.printf("%-10d%-15d%n", i, fibonacci.fibmemo(i));
+            }
+        }
+        scanner.close();
+    }
+}
